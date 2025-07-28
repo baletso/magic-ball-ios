d12ad8e77346da3497886ee8545259d706078518
Bárbara Letelier <barbaraletelier@MacBook-Air-de-Barbara.local>
2025-07-28 16:54:35 -0400

    feat: Add Spanish UX writing, improved instructions, and highlighted key word in subtitle ✨
    - Updated main title and subtitle to Spanish for better local experience
    - Subtitle now features one bold key word and sparkles emoji for emotional engagement
    - Improved line breaks and layout to support all screen sizes

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var magicTitle: UILabel!
     
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
     @IBOutlet weak var centralBall: UIImageView!
     
     let ballArray = [
@@ -25,7 +27,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var magicButton: UIButton!
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        
+        let baseText = "Piensa una pregunta y toca el botón para conocer la respuesta mágica ✨"
+        
+        let attributedText = NSMutableAttributedString(string: baseText)
+        
+        if let boldRange = baseText.range(of: "respuesta mágica ✨") {
+                let nsRange = NSRange(boldRange, in: baseText)
+                attributedText.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 18), range: nsRange)
+            }
+        
+        subtitleLabel.attributedText = attributedText
+        subtitleLabel.numberOfLines = 0
+        
+        super.viewDidLoad()
             centralBall.image = UIImage(named: "ball1")
         }
     
