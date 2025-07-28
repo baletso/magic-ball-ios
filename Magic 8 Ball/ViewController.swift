cc09d899aa4f886153b3d0c8187c20012a479c71
Bárbara Letelier <barbaraletelier@MacBook-Air-de-Barbara.local>
2025-07-28 13:44:48 -0400

    Improve UI layout: centered Magic Ball, added Auto Layout constraints, fixed scaling issues, and ensured proper spacing for all elements.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    @IBOutlet weak var magicTitle: UILabel!
+    
+    @IBOutlet weak var centralBall: UIImageView!
+    
+    let ballArray = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5")
+    ]
+    
+    @IBOutlet weak var magicButton: UIButton!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            centralBall.image = UIImage(named: "ball1")
+        }
+    
+    @IBAction func magicButtonPressed(_ sender: UIButton) {
+            let randomIndex = Int.random(in: 0...4)
+            centralBall.image = ballArray[randomIndex]
+        }
+    
 }
 
